00a9d97d2a545ac18173405114daaaa75012a7e9
Jehoon <limjhoon8@gmail.com>
2024-06-25 12:47:06 +0900

    # 4. JPA Query method, native SQL

--- src/test/java/com/spring/jpastudy/chap01/repository/ProductRepositoryTest.java
+++ src/test/java/com/spring/jpastudy/chap01/repository/ProductRepositoryTest.java
@@ -10,7 +10,6 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 import static com.spring.jpastudy.chap01.entity.Product.Category.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -120,6 +119,30 @@ class ProductRepositoryTest {
         assertEquals(4, productList.size());
     }
 
+
+    @Test
+    @DisplayName("2번 상품의 이름과 카테고리를 수정한다")
+    void modifyTest() {
+        //given
+        Long id = 2L;
+        String newName = "청소기";
+        Product.Category newCategory = ELECTRONIC;
+        //when
+
+        /*
+            jpa에서는 수정메서드를 따로 제공하지 않습니다.
+            단일 조회를 수행한 후 setter를 통해 값을 변경하고
+            다시 save를하면 INSERT대신에 UPDATE문이 나갑니다.
+         */
+        Product product = productRepository.findById(id).orElse(null);
+        product.setName(newName);
+        product.setCategory(newCategory);
+
+        Product saved = productRepository.save(product);
+
+        //then
+        assertEquals(newName, saved.getName());
+    }
 
 
 

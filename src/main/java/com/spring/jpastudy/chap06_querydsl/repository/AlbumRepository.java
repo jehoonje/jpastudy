e764390e75a2d56e2aa5bb95f21d3ead91c8079a
Jehoon <limjhoon8@gmail.com>
2024-07-03 12:18:21 +0900

    # JPA

--- src/main/java/com/spring/jpastudy/chap06_querydsl/repository/AlbumRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.jpastudy.chap06_querydsl.repository;
-
-import com.spring.jpastudy.chap06_querydsl.entity.Album;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlbumRepository extends JpaRepository<Album, Long> {
-}
